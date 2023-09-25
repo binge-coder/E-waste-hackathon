5cf1c7da067c336cd4897d0fce7e912ce269b553
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2023-09-25 22:57:44 +0530

    animated hamburger icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,11 +47,34 @@ const Navbar = () => {
                 </div>
                 {/* hamburger button */}
                 <div className='-mr-2 flex md:hidden'>
-                    <button type='button' onClick={handleMenu} className='inline-flex items-center justify-center p-2 rounded-md ring-1 ring-secondary text-secondary hover:text-stroke-dark hover:bg-secondary'>
+                {/*     <button type='button' onClick={handleMenu} className='inline-flex items-center justify-center p-2 rounded-md ring-1 ring-secondary text-secondary hover:text-stroke-dark hover:bg-secondary'>
                         <span className='sr-only'>Open main menu</span>
                         {open == true ? <FaTimes className='w-8 h-8'/> : <FaBars className='w-8 h-8'/>}
                     </button>
-                </div>
+                </div> */}
+                    
+                   
+                    <button
+  className={`relative group ${open ? 'open' : ''} md:hidden -mr-2`}
+  type="button"
+  onClick={handleMenu}
+>
+  <div className="relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-secondary ring-0 ring-secondary hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
+    <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
+      <div className={`bg-primary h-[2px] w-7 transform transition-all duration-300 origin-left ${open ? 'group-focus:translate-x-10' : ''}`}></div>
+      <div className={`bg-primary h-[0.5px] w-5 rounded transform transition-all duration-300 ${open ? 'group-focus:translate-x-10 delay-75' : ''}`}></div>
+      <div className={`bg-primary h-[2px] w-7 transform transition-all duration-300 origin-left ${open ? 'group-focus:translate-x-10 delay-150' : ''}`}></div>
+
+      <div className={`absolute items-center justify-between transform transition-all duration-500 top-2.5 ${open ? '-translate-x-10 group-focus:translate-x-0 flex w-12' : ''}`}>
+        <div className={`absolute bg-primary h-[2px] w-5 transform transition-all duration-500 ${open ? 'rotate-45 delay-300 group-focus:rotate-45' : 'rotate-0'}`}></div>
+        <div className={`absolute bg-primary h-[2px] w-5 transform transition-all duration-500 ${open ? '-rotate-45 delay-300 group-focus:-rotate-45' : 'rotate-0'}`}></div>
+      </div>
+    </div>
+  </div>
+</button>
+
+
+    </div>
             </div>
         </div>
         {/* mobile menu */}
