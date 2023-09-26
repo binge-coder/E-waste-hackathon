379fdde12ae92850c6659ba51f2ab4a6edf3ad35
Amanbig <93067432+Amanbig@users.noreply.github.com>
2023-09-26 12:43:53 +0530

    2/2 Part of Backend
    Added server.js

---
+++ server.js
@@ -0,0 +1,69 @@
+import express from "express";
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const assetsRouter = express.Router();
+
+const imageRegex = /\/.+\.(svg|png|jpg|png|jpeg)$/;
+const videoRegex = /\/.+\.(mp4|ogv)$/;
+
+app.set("view engine", "ejs"); // Set the view engine here
+
+app.use(express.json());
+
+assetsRouter.get(imageRegex, (req, res) => {
+  const filePath = req.path;
+  res.redirect(303, `http://localhost:5173/src${filePath}`);
+});
+
+assetsRouter.get(videoRegex, (req, res) => {
+  const filePath = req.path;
+  res.redirect(303, `http://localhost:5173/src${filePath}`);
+});
+
+// Serve static files from the "public" directory
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({extended:true}));
+
+// Use the assetsRouter for the "/assets" and "/src" routes
+app.use("/assets", assetsRouter);
+app.use("/src", assetsRouter);
+
+// Define your API route
+app.get("/api/v1", (req, res) => {
+  res.json({
+    project: "React and Express Boilerplate",
+    from: "Vanaldito",
+  });
+});
+
+// Serve the index.html file for all other routes
+app.get("/*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const { PORT = 5000 } = process.env;
+
+app.post("/locate", (req, res) => {
+    // Handle the data sent by the form
+    const formData = req.body;
+  
+    // You can now send this data to the "locate" component or do any other processing
+    // For now, just log the data
+    console.log("Form data received on the server:", formData);
+  
+    // Send a response back to the client (you can customize this response)
+    res.json({ message: "Form data received on the server" });
+  });
+
+app.listen(PORT, () => {
+  console.log();
+  console.log(`  App running on port ${PORT}`);
+  console.log();
+  console.log(`  > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`);
+});
