0e037b1171f6691a010462f807dfffcc313861ce
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2023-09-26 08:26:03 +0530

    fixed locate page background bug

--- src/pages/Locate.jsx
+++ src/pages/Locate.jsx
@@ -55,7 +55,7 @@ const Locate = () => {
   const navbarHeight = 80; // 80px
   const largestDivHeight = `calc(100vh - ${navbarHeight}px)`;
 
-  const [isMobile, setIsMobile] = useState(false)
+  const [isMobile, setIsMobile] = useState(true)
  
 //choose the screen size 
 const handleResize = () => {
